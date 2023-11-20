2004be16311a163489b40e3932d2c0ef01e32c77
Ehsan Tabari <etabari@users.noreply.github.com>
2023-11-19 16:59:01 -0800

    Add 800ms delay

--- js/biotech.js
+++ js/biotech.js
@@ -1,1 +1,7 @@
-for(c of document.getElementsByClassName("body")[0].getElementsByTagName("p"))c.style.display="block";for(c of document.getElementsByClassName("paywall"))c.style.display="none";for(c of document.getElementsByClassName("header"))c.style.display="none";for(c of document.getElementsByClassName("premium"))c.style.display="none";
+setTimeout(function(){
+  for(c of document.getElementsByClassName("body")[0].getElementsByTagName("p"))c.style.display="block";
+  for(c of document.getElementsByClassName("paywall"))c.style.display="none";
+  for(c of document.getElementsByClassName("header"))c.style.display="none";
+  for(c of document.getElementsByClassName("premium"))c.style.display="none";
+  x=document.createElement('span');x.innerText="biotech-news:";p[0].insertBefore(x,p[0].firstChild);
+},800);
